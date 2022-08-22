4a7f9876dd2ad3c82d0484086e75915bbc52f327
Programadorwes <wesrodriguez@outlook.com.br>
2022-08-22 16:20:52 -0300

    Adicionando login e register com composer

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -32,13 +32,40 @@
                     <li class="nav-item">
                         <a href="/events/create" class="nav-link">Criar eventos</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link">Entrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link">Cadastrar</a>
-                    </li>
+<!--    O AUTH SERVE PARA REDIRECIONAR O EVENTOS LOGIN   E SAIR DO LOG  -->
+                
+                @auth
+                <li class="nav-item">
+                        <a href="/dashboard" class="nav-link">Meus eventos</a>
+                </li>
 
+                <li class="nav-item">
+                    <form action="logout" method= "POST">
+                        @csrf
+                        <a href="/logout"
+                            class="nav-link"
+                            onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                            Sair
+                        </a>
+                    </form>
+            
+                </li>
+                @endauth 
+
+
+
+<!--                    O GUEST SERVE PARA OCULTAR OS ICONES DE ENTRAR E REGISTRAR                        -->
+                @guest
+
+                <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
+                </li>
+
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link">Cadastrar</a>
+                    </li>
+                @endguest
                 </ul>
             </div>
         </nav>
